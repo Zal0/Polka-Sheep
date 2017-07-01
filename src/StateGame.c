4b990723d8ff5e882d9ad087b180776adf1edf8d
Zalo <cebolleto@gmail.com>
2017-07-01 19:33:32 +0200

    Console initiliazation for debug purposes

--- src/StateGame.c
+++ src/StateGame.c
@@ -2,12 +2,14 @@
 #include "StateGame.h"
 UINT8 bank_STATE_GAME = 2;
 
+#include "..\res\src\font.h"
 #include "..\res\src\tiles.h"
 #include "..\res\src\map.h"
 
 #include "ZGBMain.h"
 #include "Scroll.h"
 #include "SpriteManager.h"
+#include "Print.h"
 
 UINT8 collisions[] = {1, 0};
 
@@ -26,6 +28,8 @@ void Start_STATE_GAME() {
 	InitScrollTiles(0, 8, tiles, 3);
 	InitScroll(mapWidth, mapHeight, map, collisions, 0, 3);
 	SHOW_BKG;
+
+	INIT_CONSOLE(font, 3, 2);
 }
 
 void Update_STATE_GAME() {
