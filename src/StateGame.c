c2efcaa4c36058a91132cac2241ae9b13175ea13
Zalo <cebolleto@gmail.com>
2017-07-01 20:57:04 +0200

    loading window tiles

--- src/StateGame.c
+++ src/StateGame.c
@@ -5,6 +5,7 @@ UINT8 bank_STATE_GAME = 2;
 #include "..\res\src\font.h"
 #include "..\res\src\tiles.h"
 #include "..\res\src\map.h"
+#include "..\res\src\window.h"
 
 #include "ZGBMain.h"
 #include "Scroll.h"
@@ -43,6 +44,7 @@ void Start_STATE_GAME() {
   WY_REG = (144 - (2 << 3));\
 	scroll_h_border = 2 << 3;\
 	SHOW_WIN;
+	InitWindow(0, 0, 20, 3, window, 3, 0);
 #else
 	INIT_CONSOLE(font, 3, 2);
 #endif
