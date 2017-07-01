d558f3d899f926c2532e1b6af35ff47c8c60696a
sergeeo <sergeeo@gmail.com>
2017-07-01 17:47:39 +0200

    test map: sticky tiles: 2-5 start: 6 end: 7

--- res/src/tiles.h
+++ res/src/tiles.h
@@ -10,7 +10,7 @@
   Compression          : None.
   Counter              : None.
   Tile size            : 8 x 8
-  Tiles                : 0 to 1
+  Tiles                : 0 to 7
 
   Palette colors       : None.
   SGB Palette          : None.
