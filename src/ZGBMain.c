a763728473d8375c91a533a324d68ef5d0ee0c42
Zalo <cebolleto@gmail.com>
2017-07-04 20:08:30 +0200

    removed S from maps

--- src/ZGBMain.c
+++ src/ZGBMain.c
@@ -3,7 +3,10 @@
 #include "Math.h"
 UINT8 GetTileReplacement(UINT8* tile_ptr, UINT8* tile) {
 	if(current_state == STATE_GAME) {
-		if(U_LESS_THAN(255 - (UINT16)*tile_ptr, N_SPRITE_TYPES)) {
+		if(
+			U_LESS_THAN(255 - (UINT16)*tile_ptr, N_SPRITE_TYPES) ||
+			*tile_ptr == 4
+		) {
 			*tile = 0;
 			return 255 - (UINT16)*tile_ptr;
 		}
