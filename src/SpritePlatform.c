3382fdcd45753fe19b08556decac74ae301ee658
Zal0 <cebolleto@gmail.com>
2021-08-17 07:09:11 +0200

    Spawn sprites checking coll_h

--- src/SpritePlatform.c
+++ src/SpritePlatform.c
@@ -23,35 +23,34 @@ void Start_SpritePlatform() {
 	struct PlatformCustomData* data = (struct PlatformCustomData*)THIS->custom_data;
 	data->frame_accum = 0;
 
-	tile = GetScrollTile(tile_x + 1, tile_y + 1);
+	tile = GetScrollTile(tile_x + 1, tile_y);
 	if(U_LESS_THAN(START_TILE - 1 , tile) && U_LESS_THAN(tile, START_TILE + 6)) {
 		data->vy = -1;
 		data->vx =  0;
 		THIS->x += 4;
 	} else {
-		tile = GetScrollTile(tile_x - 1, tile_y + 1);
+		tile = GetScrollTile(tile_x - 1, tile_y);
 		if(U_LESS_THAN(START_TILE - 1 , tile) && U_LESS_THAN(tile, START_TILE + 6)) {
 			data->vy = 1;
 			data->vx = 0;
 			THIS->x -= 12;
 		} else {
-			tile = GetScrollTile(tile_x, tile_y + 2);
+			tile = GetScrollTile(tile_x, tile_y + 1);
 			if(U_LESS_THAN(START_TILE - 1 , tile) && U_LESS_THAN(tile, START_TILE + 6)) {
 				data->vy =  0;
 				data->vx = -1;
-				THIS->y += 12;
+				THIS->y += 7;
 			} else {
-				tile = GetScrollTile(tile_x, tile_y);
+				tile = GetScrollTile(tile_x, tile_y - 1);
 				if(U_LESS_THAN(START_TILE - 1 , tile) && U_LESS_THAN(tile, START_TILE + 6)) {
 					data->vy = 0;
 					data->vx = 1;
-					THIS->y -= 4;
+					THIS->y -= 9;
 				}
 			}
 		}
 	}
 
-	THIS->y += 5;
 	THIS->lim_x = 100u;
 	THIS->lim_y = 100u;
 }
